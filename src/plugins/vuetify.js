6703f237f17cc54f3bf8688dc3acc0ce1505a5f3
apm29 <yjw999wow@163.com>
2020-04-17 16:25:21 +0800

    config

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import zhHans from 'vuetify/es5/locale/zh-Hans';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+      options: {
+        customProperties: true,
+      },
+    themes: {
+      light: {
+        primary: "#FB8C00",
+        secondary: "#FFB74D",
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+      },
+      dark: {
+        primary: '#2196F3',
+        secondary: '#424242',
+        accent: '#FF4081',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+      },
+    },
+  },
+    lang: {
+      locales: { zhHans },
+      current: 'zh-Hans',
+    },
+});
