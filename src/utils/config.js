cbc2121b6b8de42b75ec55b85afeead2792fc24a
yingjiawei <yjw999wow@163.com>
2020-04-24 23:56:06 +0800

    tiny mce

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 export default {
   baseUrl: process.env.NODE_ENV === 'development'
     ? 'http://localhost:9998/'
-    : 'http://jiayupearl.shop:9998/',
+    : 'http://jiayupearl.shop/',
   tokenKey: 'token',
   fileBaseUrl: 'http://jiayu-pearl-mall.oss-cn-beijing.aliyuncs.com/',
 }
