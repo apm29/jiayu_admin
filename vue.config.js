17ddbdcd118ccd55bcb64395c769a94893d7ae59
yingjiawei <yjw999wow@163.com>
2020-04-24 23:21:07 +0800

    tiny mce

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: './',
+  publicPath: '/manager/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
