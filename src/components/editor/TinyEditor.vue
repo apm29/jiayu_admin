cbb7b80fc9ffb6ba6f6e2b60d6dfc2292e9dd94c
apm29 <yjw999wow@163.com>
2020-04-27 15:25:56 +0800

    tiny mce 修改

--- src/components/editor/TinyEditor.vue
+++ src/components/editor/TinyEditor.vue
@@ -1,10 +1,84 @@
 <template>
-
+    <editor
+            v-if="show"
+            v-model="text"
+            :disabled="disabled"
+            :init="settings"
+    />
 </template>
 
 <script>
+  import Editor from '@tinymce/tinymce-vue'
   export default {
-    name: 'TinyEditor',
+    name: 'TinyEditor',components: {
+      Editor
+    },
+    props:{
+      value:{
+        type:String,
+        default:''
+      },
+      disabled: Boolean
+    },
+    modal:{
+      event: 'input',
+    },
+    data: function () {
+      return {
+        text:this.value,
+        show: true,
+        settings: {
+          height: 500,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+          ],
+          language: 'zh_CN',
+          skin_url: this.$store.state.app.dark ? './tinymce/skins/ui/oxide-dark' : './tinymce/skins/ui/oxide',
+          content_css: this.$store.state.app.dark ? 'dark' : 'writer',
+          images_upload_handler: this.upload
+        }
+      }
+    },
+    watch: {
+      '$store.state.app.dark': {
+        immediate: true,
+        handler: function (val) {
+          this.settings.skin_url = val ?  './tinymce/skins/ui/oxide-dark' : './tinymce/skins/ui/oxide'
+          this.settings.content_css = val ? 'dark' : 'writer'
+          this.show = false
+          this.$nextTick(() => {
+            this.show = true
+          })
+        }
+      },
+      text:{
+        immediate:true,
+        deep:true,
+        handler:function (val) {
+          this.$emit('input',val)
+        }
+      }
+    },
+    methods: {
+      upload: function (blobInfo, success, failure) {
+        let formData = new FormData()
+        formData.append('file', blobInfo.blob())
+        this.$remote.upload({
+          formData: formData,
+          url: '/files/upload',
+        }).then((res) => {
+          success('http://jiayu-pearl-mall.oss-cn-beijing.aliyuncs.com/'+res.Data.filePath)
+        }).catch((e) => {
+          this.$notify({
+            text: e,
+            type: 'error',
+          })
+          failure(e)
+        })
+      },
+    }
   }
 </script>
 
--- src/views/goods/GoodsCreate.vue
+++ src/views/goods/GoodsCreate.vue
@@ -1,66 +1,26 @@
 <template>
     <v-card class="fill-height d-flex flex-column">
-        <editor
-                v-if="show"
-                :init="settings"
-        />
+        <tiny-editor v-model="text" @textChange="onTextChange"/>
     </v-card>
+
 </template>
 
 <script>
-  import Editor from '@tinymce/tinymce-vue'
 
+  import TinyEditor from '@/components/editor/TinyEditor'
+  import VCascader from '@/components/cascader/VCascader'
   export default {
     name: 'GoodsCreate',
-    components: {
-      Editor
-    },
-    data: function () {
-      return {
-        show: true,
-        settings: {
-          height: 500,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-          ],
-          language: 'zh_CN',
-          skin_url: this.$store.state.app.dark ? './tinymce/skins/ui/oxide-dark' : './tinymce/skins/ui/oxide',
-          content_css: this.$store.state.app.dark ? 'dark' : 'writer',
-          images_upload_handler: this.upload
+    components: { VCascader, TinyEditor },
+    data:function () {
+        return {
+          selection:[],
+          text:'',
         }
-      }
     },
-    watch: {
-      '$store.state.app.dark': {
-        immediate: true,
-        handler: function (val) {
-          this.settings.skin_url = val ?  './tinymce/skins/ui/oxide-dark' : './tinymce/skins/ui/oxide'
-          this.settings.content_css = val ? 'dark' : 'writer'
-          this.show = false
-          this.$nextTick(() => {
-            this.show = true
-          })
-        }
-      }
-    },
-    methods: {
-      upload: function (blobInfo, success, failure) {
-        let formData = new FormData()
-        formData.append('file', blobInfo.blob())
-        this.$remote.upload({
-          formData: formData,
-          url: '/files/upload',
-        }).then((res) => {
-          success('http://jiayu-pearl-mall.oss-cn-beijing.aliyuncs.com/'+res.Data.filePath)
-        }).catch((e) => {
-          this.$notify({
-            text: e,
-            type: 'error',
-          })
-          failure(e)
-        })
+    methods:{
+      onTextChange:function (val) {
+        console.log(val)
       }
     }
   }
