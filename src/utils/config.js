e5c4be8e266b861250b44e8ab31e40391cd573c9
apm29 <yjw999wow@163.com>
2020-05-19 14:14:01 +0800

    https

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,10 @@
 export default {
-  baseUrl: process.env.NODE_ENV === 'development'
+  baseUrl: process.env.NODE_ENV === '1development'
     ? 'http://localhost:9998/java/'
-    : 'http://jiayupearl.shop/java/',
+    : 'https://jiayupearl.shop/java/',
   uploadBaseUrl: process.env.NODE_ENV === 'development'
-    ? 'http://jiayupearl.shop/java/'
-    : 'http://jiayupearl.shop/java/',
+    ? 'https://jiayupearl.shop/java/'
+    : 'https://jiayupearl.shop/java/',
   tokenKey: 'token',
   fileBaseUrl: 'http://jiayu-pearl-mall.oss-cn-beijing.aliyuncs.com/',
   fileThumbnailParamsSmall: '?x-oss-process=image/resize,h_48,w_48',
